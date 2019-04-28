23868a3ecf5bc3d90425340e04d1c255c6c04f1c
agon-lohaj <agon.lohaj@gmail.com>
2019-04-28 22:05:33 +0200

    exercise added at version 4.1

--- src/pages/lecture4/Exercise.js
+++ src/pages/lecture4/Exercise.js
@@ -13,6 +13,7 @@ import {
   randomGroupsOfLength,
   uniqueValues
 } from 'utils/DataGenerator'
+import { TextField, FormControlLabel, Checkbox } from "@material-ui/core";
 
 const styles = ({ typography, size }) => ({
   content: {
@@ -27,11 +28,19 @@ const styles = ({ typography, size }) => ({
 })
 
 const groups = uniqueValues(randomGroupsOfLength(5))
+// ["Developer", "Loop"]
 const words = randomWordsOfLength(50)
 const values = randomPositiveValues(50)
 
 const items = values.map((value, index) => ({ group: groups[index % groups.length], word: words[index % words.length], value, inStock: index % 5 === 0 }))
-
+/*
+[{
+  group: '',
+  word: '',
+  value: 43,
+  inStock: false
+}]
+*/
 const grouped = groups.map(group => {
   return {
     group,
@@ -39,10 +48,69 @@ const grouped = groups.map(group => {
   }
 })
 
+const Card = (props) => {
+  return (
+    <div style={{
+      background: 'white', 
+      borderRadius: 8, 
+      width: 320, 
+      height: 240, 
+      display: 'flex', 
+      flexFlow: 'column wrap',
+      margin: 8,
+      padding: 16}}>
+      <div style={{fontSize: 16, fontWeight: 'bold', width: '100%'}}>
+        {props.title}
+      </div>
+      <div style={{flex: 1, width: '100%', overflowY: 'auto'}}>
+        {props.children}
+      </div>
+    </div>
+  )
+}
+
 class Exercise extends React.Component {
+
+  constructor (props) {
+    super (props)
+    this.state = {
+      items: grouped,
+      search: '',
+      inStock: false
+    }
+  }
+  onValueChanged = (event) => {
+    this.setState({
+      search: event.target.value
+    })
+  }
+
+  onToggle = () => {
+    this.setState((prevState) => ({
+      inStock: !prevState.inStock
+    }))
+  }
 
   render() {
     const { classes, section } = this.props
+    const { items, search, inStock } = this.state
+
+    const filtered = items
+    .map(item => {
+      return {
+        ...item,
+        children: item.children
+          .filter(child => {
+            return child.word.includes(search)
+          }).filter(child => {
+            return !inStock || child.inStock === inStock
+          })
+      }
+    }).filter(next => {
+      return next.children.length > 0
+    })
+    
+    
     return (
       <Fragment>
         <Typography variant={'heading'}>
@@ -53,6 +121,20 @@ class Exercise extends React.Component {
           <Divider />
         </Typography>
         <div className={classes.content}>
+        <TextField fullWidth onChange={this.onValueChanged} label="Search" value={search} />
+        <FormControlLabel onChange={this.onToggle} style={{width: '100%'}} control={
+          <Checkbox checked={inStock} />
+        } label="In Stock"></FormControlLabel>
+        {filtered.map((item, index) => {
+          return <Card title={item.group} key={index}>
+            {item.children.map((next, index) => {
+              return <div key={`child-${index}`} style={{padding: 4, paddingRight: 8, width: '100%', display: 'flex', justifyContent: 'space-between', flexFlow: 'row wrap'}}>
+                <div>Word: {next.word}</div>
+                <div>{next.value} {next.inStock ? 'In Stock' : 'Sold Out'}</div>
+              </div>
+            })}
+          </Card>
+        })}
         </div>
       </Fragment>
     )
