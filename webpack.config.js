59a59a372cc5bc295cefe6589579c8ff45ae02fd
agon-lohaj <agon.lohaj@gmail.com>
2019-05-27 00:49:36 +0200

    updated babel loader

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.(js|jsx)$/,
+                test: /\.m?js$/,
                 exclude: /node_modules/,
                 use: {
                     loader: "babel-loader"
